6a7096d187e6ce38f72a8ca150b5f3c5091c9d11
Ph4ra0hXX <ph4ra0hx@gmail.com>
2024-06-20 19:21:33 -0300

    teste

--- src/pages/burger.vue
+++ src/pages/burger.vue
@@ -34,7 +34,7 @@ export default {
         { nome: "Ovo", preco: 2.0, quantidade: 0 },
         { nome: "Cebola Caramelizada", preco: 1.0, quantidade: 0 },
         { nome: "Maionese Golds", preco: 0.0, quantidade: 0 },
-        { nome: "Presunto", preco: 1.0, quantidade: 0 },
+        { nome: "Presunto", preco: 2.0, quantidade: 0 },
         { nome: "Calabresa", preco: 2.0, quantidade: 0 },
         { nome: "Batata Palha", preco: 1.0, quantidade: 0 },
         { nome: "Molho Barbecue", preco: 1.0, quantidade: 0 },
