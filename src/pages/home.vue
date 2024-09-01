1cffacde2d1a8501af1e35381cca23b5aa31b2e3
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2024-09-01 13:19:58 -0300

    Update home.vue

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -23,8 +23,8 @@ function isWednesdayOrThursday() {
 
 <template>
   <h2 id="fechados">No momento estamos fechados!</h2>
-   <Card
-   <!-- v-if="isWednesdayOrThursday()"
+ <!--  <Card
+    v-if="isWednesdayOrThursday()"
     v-for="data2 in cardData2"
     :data="data2"
   />
