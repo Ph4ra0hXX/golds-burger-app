61710bde88479cba9c4bdde51f212bf6a6b97ef6
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2024-10-16 17:15:58 -0300

    Update produtos.js

--- src/store/produtos.js
+++ src/store/produtos.js
@@ -129,14 +129,14 @@ export const produtosStore2 = defineStore("produto2", {
           img: "food2.webp",
           descricao: "Monte do seu jeito",
           url: "/macarronada",
-        },
+        },*/
        {
           id: 6,
           nome: "BATATA FRITA",
           img: "food3.webp",
           descricao: "No ponto certo",
           url: "/batata",
-        },*/
+        },
         {
           id: 7,
           nome: "SOBREMESAS",
