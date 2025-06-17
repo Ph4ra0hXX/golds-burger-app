165ffdcdfec41b7ec70b23f7b23e13c8fc7c9dbd
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2025-06-17 12:25:13 -0300

    Update bebidas.vue

--- src/pages/bebidas.vue
+++ src/pages/bebidas.vue
@@ -13,7 +13,7 @@ export default {
     const Bebidas = ref({
       tipos: [
         { nome: "Coca Litro", quantidade: 0, preco: 9.0 },
-        // { nome: "Guaraná Litro", quantidade: 0, preco: 8.0 },
+        { nome: "Guaraná Litro", quantidade: 0, preco: 8.0 },
         { nome: "Cajuina Litro", quantidade: 0, preco: 9.0 },
         { nome: "Coca Zero Litro", quantidade: 0, preco: 9.0 },
        // { nome: "Coca 600ml", quantidade: 0, preco: 7.0 },
@@ -22,7 +22,7 @@ export default {
         { nome: "Cajuína Zero Lata", quantidade: 0, preco: 5.0 },
         { nome: "Fanta Caju Lata", quantidade: 0, preco: 5.0 },
         { nome: "Coca Zero Lata", quantidade: 0, preco: 5.0 },
-       // { nome: "Guaraná Lata", quantidade: 0, preco: 5.0 },
+        { nome: "Guaraná Lata", quantidade: 0, preco: 5.0 },
         { nome: "Laranja Lata", quantidade: 0, preco: 5.0 },
         { nome: "Uva Lata", quantidade: 0, preco: 5.0 },
         { nome: "São Geraldo Lata", quantidade: 0, preco: 5.0 },
