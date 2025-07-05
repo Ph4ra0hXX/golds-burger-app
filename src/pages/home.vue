98849b30b446f1be7203cb43a50ec43b7a941ea5
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2025-07-05 18:32:21 -0300

    Update home.vue

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -45,12 +45,12 @@ function isSegunda() {
 </script>
 
 <template>
-<!-- <h2 id="fechados">No momento estamos fechados!</h2> -->
-<Card v-if="isWednesday()" v-for="data in diaDeMacarronada" :data="data" /> 
+ <h2 id="fechados">No momento estamos fechados!</h2>
+<!-- <Card v-if="isWednesday()" v-for="data in diaDeMacarronada" :data="data" /> 
 
  <Card v-if="isThursday()" v-for="data in diaDeSuper" :data="data" />
 
-  <Card v-if="isNotWednesdayOrThursday()" v-for="data in nenhum" :data="data" />
+  <Card v-if="isNotWednesdayOrThursday()" v-for="data in nenhum" :data="data" /> -->
 </template>
 
 <style>
