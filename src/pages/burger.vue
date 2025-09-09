4fa0558e60b777cea68b85b92c30a656974311e3
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2025-09-09 18:18:59 -0300

    Update burger.vue

--- src/pages/burger.vue
+++ src/pages/burger.vue
@@ -27,7 +27,7 @@ export default {
       ],
       Carne: [
         { nome: "Frango Desfiado", preco: 4.0, quantidade: 0 },
-      //  { nome: "Hambúrguer Caseiro", preco: 6.0, quantidade: 0 },
+        { nome: "Hambúrguer Caseiro", preco: 6.0, quantidade: 0 },
         { nome: "Carne de Sol desfiada", preco: 5.0, quantidade: 0 },
         { nome: "Hambúrguer", preco: 2.0, quantidade: 0 },
         { nome: "Filé de frango", preco: 4.0, quantidade: 0 },
