fc8d8d317a682100dc27b3a3b2c97aee6dbaf2af
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2025-12-23 21:34:35 -0300

    Update burger.vue

--- src/pages/burger.vue
+++ src/pages/burger.vue
@@ -13,8 +13,8 @@ export default {
     const Burger = ref({
       Pao: [
         { nome: "Pão Árabe", preco: 3.0, quantidade: 0 },
-        { nome: "Pão Bola", preco: 2.0, quantidade: 0 },
-        { nome: "Pão Brioche", preco: 3.0, quantidade: 0 },
+       // { nome: "Pão Bola", preco: 2.0, quantidade: 0 },
+       // { nome: "Pão Brioche", preco: 3.0, quantidade: 0 },
       ],
       Queijo: [
         { nome: "Cheddar", preco: 4.0, quantidade: 0 },
