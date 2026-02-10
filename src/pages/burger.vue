dcc1ec1a90a680ce9803cc4d2ebde32806a55189
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2026-02-10 08:52:44 -0300

    Uncomment options for bread and meat in burger menu

--- src/pages/burger.vue
+++ src/pages/burger.vue
@@ -15,7 +15,7 @@ export default {
         { nome: "🟢 Pão Bola", preco: 3.0, quantidade: 0 },
         { nome: "Pão Árabe", preco: 3.0, quantidade: 0 },
         { nome: "Pão Integral", preco: 4.0, quantidade: 0 },
-        //{ nome: "Pão Brioche", preco: 3.0, quantidade: 0 },
+        { nome: "Pão Brioche", preco: 3.0, quantidade: 0 },
       ],
       Queijo: [
         { nome: "Cheddar", preco: 4.0, quantidade: 0 },
@@ -27,7 +27,7 @@ export default {
         { nome: "Cream cheese", preco: 3.0, quantidade: 0 },
       ],
       Carne: [
-       // { nome: "🟢 Carne de Sol", preco: 6.0, quantidade: 0 },
+        { nome: "🟢 Carne de Sol", preco: 6.0, quantidade: 0 },
         { nome: "🟢 Frango Desfiado", preco: 5.0, quantidade: 0 },
         { nome: "🟢 Hambúrguer", preco: 3.0, quantidade: 0 },
         { nome: "🟢 Carne Caseira", preco: 7.0, quantidade: 0 },
