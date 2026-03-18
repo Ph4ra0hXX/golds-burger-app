5716b38032b0798f6d9f083ca7cc0bdf2db5b100
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2026-03-18 07:00:22 -0300

    Add integral and brioche bread options

--- src/pages/burger.vue
+++ src/pages/burger.vue
@@ -14,8 +14,8 @@ export default {
       Pao: [
         { nome: "🟢 Pão Bola", preco: 3.0, quantidade: 0 },
         { nome: "Pão Árabe", preco: 3.0, quantidade: 0 },
-        //{ nome: "Pão Integral", preco: 4.0, quantidade: 0 },
-       // { nome: "Pão Brioche", preco: 3.0, quantidade: 0 },
+        { nome: "Pão Integral", preco: 4.0, quantidade: 0 },
+        { nome: "Pão Brioche", preco: 3.0, quantidade: 0 },
       ],
       Queijo: [
          { nome: "Cheddar", preco: 4.0, quantidade: 0 },
