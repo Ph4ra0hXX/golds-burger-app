3fcab21b0a709741637f7a2795f8e1f2a1d0d2f8
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2026-04-25 20:33:08 -0300

    Update burger.vue

--- src/pages/burger.vue
+++ src/pages/burger.vue
@@ -27,7 +27,7 @@ export default {
         { nome: "Cream cheese", preco: 3.0, quantidade: 0 },
       ],
       Carne: [
-        { nome: "🟢 Carne de Sol", preco: 6.0, quantidade: 0 },
+      //  { nome: "🟢 Carne de Sol", preco: 6.0, quantidade: 0 },
         { nome: "🟢 Frango Desfiado", preco: 5.0, quantidade: 0 },
         { nome: "🟢 Hambúrguer", preco: 3.0, quantidade: 0 },
         { nome: "🟢 Carne Caseira", preco: 7.0, quantidade: 0 },
