9a191e22b808afdcb331cf40f923e7d95b06fb5a
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2026-06-10 16:12:45 -0300

    Update sobremesas.vue

--- src/pages/sobremesas.vue
+++ src/pages/sobremesas.vue
@@ -13,8 +13,8 @@ export default {
     const Sobremesas = ref({
       tipos: [
         { nome: "Pudim 120G", quantidade: 0, preco: 7.0 },
-     /*   { nome: "Torta de abacaxi 120G", quantidade: 0, preco: 7.0 },
-        { nome: "Brownie de Chocolate Branco", quantidade: 0, preco: 6.0 },
+        { nome: "Torta de abacaxi 120G", quantidade: 0, preco: 7.0 },
+     /*   { nome: "Brownie de Chocolate Branco", quantidade: 0, preco: 6.0 },
         { nome: "Brownie de Amendoim", quantidade: 0, preco: 6.0 },
         { nome: "Brownie Tradicional", quantidade: 0, preco: 6.0 },
         { nome: "Brownie de Creme de Avelã", quantidade: 0, preco: 6.0 },
