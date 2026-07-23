fed6c940a5b53091799a644a1e1a5f7956645677
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2026-07-23 18:03:42 -0300

    Update bebidas.vue

--- src/pages/bebidas.vue
+++ src/pages/bebidas.vue
@@ -41,6 +41,7 @@ export default {
         { nome: "Suco Goiaba", quantidade: 0, preco: 5.0 },
         { nome: "Suco Cajá", quantidade: 0, preco: 5.0 },
         { nome: "Suco Acerola", quantidade: 0, preco: 5.0 },
+        { nome: "Suco de Manga", quantidade: 0, preco: 5.0 },
         { nome: "Suco Maracujá", quantidade: 0, preco: 6.0 },
         { nome: "Cerveja Heineken", quantidade: 0, preco: 10.0 },
         { nome: "Cerveja Budweiser", quantidade: 0, preco: 9.0 },
