dcffb91ae6d7c177aa07ed83a043700ba49d741a
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2026-08-01 14:05:07 -0300

    Comment out Guaraná Lata in bebidas.vue
    Comment out the Guaraná Lata entry in bebidas list.

--- src/pages/bebidas.vue
+++ src/pages/bebidas.vue
@@ -24,7 +24,7 @@ export default {
         { nome: "Cajuína Zero Lata", quantidade: 0, preco: 5.0 },
         { nome: "Fanta Caju Lata", quantidade: 0, preco: 5.0 },
         { nome: "Coca Zero Lata", quantidade: 0, preco: 5.0 },
-        { nome: "Guaraná Lata", quantidade: 0, preco: 5.0 },
+     //   { nome: "Guaraná Lata", quantidade: 0, preco: 5.0 },
         { nome: "Laranja Lata", quantidade: 0, preco: 5.0 },
        // { nome: "Uva Lata", quantidade: 0, preco: 5.0 },
         { nome: "São Geraldo Lata", quantidade: 0, preco: 5.0 },
